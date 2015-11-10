6c8830f1df9e949d6c47b2e4fbbe996ddd030106
Luda <ldcg1@hotmail.com>
2015-11-09 20:31:22 -0430

    Add post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,27 @@
 class PostsController < ApplicationController
 	def index
+		@post = Post.all.order('created_at DESC') #Mostrar post por fecha de creacion descendiente
 	end
 
 	def new
 	end
 
+	def create
+		@post = Post.new(post_params) #Creando nuevo objeto post con sus respectivos parametros
+		@post.save
+
+		redirect_to @post
+	end
+
+
+	def show
+		@post = Post.find(params[:id]) #Encontrar por parametro id
+		
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :body)         #Parametros de post 
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts            #Se ubica a post como un recurso a enlazar
 
-  root "posts#index"
+  root "posts#index"          #Index es la pagina raiz de la app
 end
